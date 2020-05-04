cda039e406866c37cb7acfc70ef09f0ef9bf8c8c
kirill shakhansky <bb4bbb@gmail.com>
2020-05-04 15:22:01 +0300

    initial commit

---
+++ SwiftUINavigationSample/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  SwiftUINavigationSample
+//
+//  Created by FuzzzzyBoy on 04.05.2020.
+//  Copyright © 2020 FuzzzzyBoy. All rights reserved.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
